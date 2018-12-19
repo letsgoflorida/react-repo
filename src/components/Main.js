fe0cf7a5c78086a9650ad84d65e0f70035085f40
Jaime Jacobo <jaimejacobordv@gmail.com>
2018-12-18 20:03:31 -0500

    solved merge conflicts

--- src/components/Main.js
+++ src/components/Main.js
@@ -93,7 +93,6 @@ class Main extends Component {
   render() {
     return(
       <div>
-				{/* <img src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=AIzaSyAAvIITz7r16HjYey8HVfIxfhXw29EdS0w"/> */}
 				<Profile 
 					profile={this.state.modalProfile} 
 					user={this.state.loggedUser} 
