f8d91a480eac0cf9ed10b451efcd68025d7c9ba2
Jaime Jacobo <jaimejacobordv@gmail.com>
2018-12-20 18:30:37 -0500

    added features to the create page

--- src/components/Create.js
+++ src/components/Create.js
@@ -6,13 +6,20 @@ class Create extends Component {
   render(){
     return(
       <div>
-        <div class="container">
+        <div className="flexCentered cityTitleCreate">
+          <h1>Fort Lauderdale</h1>
+        </div>
+        <div className="hotelsInfo">
+          <h2 className="createTitles">Hotels</h2>
+          <img src={require('../images/restaurant.png')} alt="logo" className="restaurantIcon"/>
+        </div>
+        <div class="container createPictureContainer">
           <div class="row blog">
             <div class="col-md-12">
-              <div id="blogCarousel" class="carousel slide" data-ride="carousel">
+              <div id="secondCarousel" class="carousel slide" data-ride="carousel">
                 <ol class="carousel-indicators">
-                  <li data-target="#blogCarousel" data-slide-to="0" class="active"></li>
-                  <li data-target="#blogCarousel" data-slide-to="1"></li>
+                  <li data-target="#secondCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#secondCarousel" data-slide-to="1"></li>
                 </ol>
                 <div class="carousel-inner">
                   <div class="carousel-item active">
@@ -68,13 +75,83 @@ class Create extends Component {
           </div>
         </div>
       </div>
-      <div class="container">
+      <div className="hotelsInfo">
+          <h2 className="createTitles">Restaurants</h2>
+          <img src={require('../images/restaurant.png')} alt="logo" className="restaurantIcon"/>
+      </div>
+      <div class="container createPictureContainer">
           <div class="row blog">
             <div class="col-md-12">
-              <div id="blogCarousel" class="carousel slide" data-ride="carousel">
+              <div id="firstBlogCarousel" class="carousel slide" data-ride="carousel">
                 <ol class="carousel-indicators">
-                  <li data-target="#blogCarousel" data-slide-to="0" class="active"></li>
-                  <li data-target="#blogCarousel" data-slide-to="1"></li>
+                  <li data-target="#firstBlogCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#firstBlogCarousel" data-slide-to="1"></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <div class="row">
+                      <div class="col-md-3">
+                        <a href="#">
+                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        </a>
+                      </div>
+                      <div class="col-md-3">
+                        <p>
+                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        </p>
+                      </div>
+                      <div class="col-md-3">
+                        <a href="#">
+                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        </a>
+                      </div>
+                      <div class="col-md-3">
+                        <a href="#">
+                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        </a>
+                      </div>
+                    </div>     
+                  </div>             
+                  <div class="carousel-item">
+                    <div class="row">
+                      <div class="col-md-3">
+                        <a href="#">
+                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        </a>
+                      </div>
+                    <div class="col-md-3">
+                      <a href="#">
+                        <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                      </a>
+                    </div>
+                    <div class="col-md-3">
+                      <a href="#">
+                        <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                      </a>
+                    </div>
+                    <div class="col-md-3">
+                      <a href="#">
+                        <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                      </a>
+                    </div>
+                  </div>
+                </div>    
+              </div> 
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hotelsInfo">
+          <h2 className="createTitles">Activities</h2>
+          <img src={require('../images/restaurant.png')} alt="logo" className="restaurantIcon"/>
+      </div>
+      <div class="container createPictureContainer">
+          <div class="row blog">
+            <div class="col-md-12">
+              <div id="thirdCarousel" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                  <li data-target="#thirdCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#thirdCarousel" data-slide-to="1"></li>
                 </ol>
                 <div class="carousel-inner">
                   <div class="carousel-item active">
