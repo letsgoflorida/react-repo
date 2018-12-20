dc97975af5cbdb3693a1e41e82432590e045f346
Jaime Jacobo <jaimejacobordv@gmail.com>
2018-12-20 18:41:10 -0500

    resolved a problem with the modal background

--- src/components/Create.js
+++ src/components/Create.js
@@ -143,7 +143,7 @@ class Create extends Component {
       </div>
       <div className="hotelsInfo">
           <h2 className="createTitles">Activities</h2>
-          <img src={require('../images/restaurant.png')} alt="logo" className="restaurantIcon"/>
+          <img src={require('../images/bike.png')} alt="logo" className="restaurantIcon"/>
       </div>
       <div class="container createPictureContainer">
           <div class="row blog">
