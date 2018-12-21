00504b1afaf0cda8f5df5af9b4d71831461b5056
Jaime Jacobo <jaimejacobordv@gmail.com>
2018-12-20 22:33:43 -0500

    footer added and corrected

--- src/components/Create.js
+++ src/components/Create.js
@@ -5,67 +5,67 @@ import React, { Component } from 'react';
 class Create extends Component {
   render(){
     return(
-      <div>
+      <div className="height">
         <div className="flexCentered cityTitleCreate">
           <h1>Fort Lauderdale</h1>
         </div>
-        <div className="hotelsInfo">
+        <div className="createInfo">
           <h2 className="createTitles">Hotels</h2>
           <img src={require('../images/hotel.png')} alt="logo" className="restaurantIcon"/>
         </div>
-        <div class="container createPictureContainer">
-          <div class="row blog">
-            <div class="col-md-12">
-              <div id="secondCarousel" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                  <li data-target="#secondCarousel" data-slide-to="0" class="active"></li>
+        <div className="container createPictureContainer">
+          <div className="row blog">
+            <div className="col-md-12">
+              <div id="secondCarousel" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                  <li data-target="#secondCarousel" data-slide-to="0" className="active"></li>
                   <li data-target="#secondCarousel" data-slide-to="1"></li>
                 </ol>
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <div class="row">
-                      <div class="col-md-3">
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <div className="row">
+                      <div className="col-md-3">
                         <a href="#">
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </a>
                       </div>
-                      <div class="col-md-3">
+                      <div className="col-md-3">
                         <p>
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </p>
                       </div>
-                      <div class="col-md-3">
+                      <div className="col-md-3">
                         <a href="#">
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </a>
                       </div>
-                      <div class="col-md-3">
+                      <div className="col-md-3">
                         <a href="#">
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </a>
                       </div>
                     </div>     
                   </div>             
-                  <div class="carousel-item">
-                    <div class="row">
-                      <div class="col-md-3">
+                  <div className="carousel-item">
+                    <div className="row">
+                      <div className="col-md-3">
                         <a href="#">
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </a>
                       </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <a href="#">
-                        <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                       </a>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <a href="#">
-                        <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                       </a>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <a href="#">
-                        <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                       </a>
                     </div>
                   </div>
@@ -75,63 +75,63 @@ class Create extends Component {
           </div>
         </div>
       </div>
-      <div className="hotelsInfo">
+        <div className="createInfo">
           <h2 className="createTitles">Restaurants</h2>
           <img src={require('../images/restaurant.png')} alt="logo" className="restaurantIcon"/>
       </div>
-      <div class="container createPictureContainer">
-          <div class="row blog">
-            <div class="col-md-12">
-              <div id="firstBlogCarousel" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                  <li data-target="#firstBlogCarousel" data-slide-to="0" class="active"></li>
+        <div className="container createPictureContainer">
+          <div className="row blog">
+            <div className="col-md-12">
+              <div id="firstBlogCarousel" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                  <li data-target="#firstBlogCarousel" data-slide-to="0" className="active"></li>
                   <li data-target="#firstBlogCarousel" data-slide-to="1"></li>
                 </ol>
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <div class="row">
-                      <div class="col-md-3">
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <div className="row">
+                      <div className="col-md-3">
                         <a href="#">
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </a>
                       </div>
-                      <div class="col-md-3">
+                      <div className="col-md-3">
                         <p>
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </p>
                       </div>
-                      <div class="col-md-3">
+                      <div className="col-md-3">
                         <a href="#">
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </a>
                       </div>
-                      <div class="col-md-3">
+                      <div className="col-md-3">
                         <a href="#">
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </a>
                       </div>
                     </div>     
                   </div>             
-                  <div class="carousel-item">
-                    <div class="row">
-                      <div class="col-md-3">
+                  <div className="carousel-item">
+                    <div className="row">
+                      <div className="col-md-3">
                         <a href="#">
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </a>
                       </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <a href="#">
-                        <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                       </a>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <a href="#">
-                        <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                       </a>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <a href="#">
-                        <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                       </a>
                     </div>
                   </div>
@@ -141,63 +141,63 @@ class Create extends Component {
           </div>
         </div>
       </div>
-      <div className="hotelsInfo">
+        <div className="createInfo">
           <h2 className="createTitles">Activities</h2>
           <img src={require('../images/bike.png')} alt="logo" className="restaurantIcon"/>
       </div>
-      <div class="container createPictureContainer">
-          <div class="row blog">
-            <div class="col-md-12">
-              <div id="thirdCarousel" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                  <li data-target="#thirdCarousel" data-slide-to="0" class="active"></li>
+        <div className="container createPictureContainer">
+          <div className="row blog">
+            <div className="col-md-12">
+              <div id="thirdCarousel" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                  <li data-target="#thirdCarousel" data-slide-to="0" className="active"></li>
                   <li data-target="#thirdCarousel" data-slide-to="1"></li>
                 </ol>
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <div class="row">
-                      <div class="col-md-3">
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <div className="row">
+                      <div className="col-md-3">
                         <a href="#">
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </a>
                       </div>
-                      <div class="col-md-3">
+                      <div className="col-md-3">
                         <p>
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </p>
                       </div>
-                      <div class="col-md-3">
+                      <div className="col-md-3">
                         <a href="#">
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </a>
                       </div>
-                      <div class="col-md-3">
+                      <div className="col-md-3">
                         <a href="#">
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </a>
                       </div>
                     </div>     
                   </div>             
-                  <div class="carousel-item">
-                    <div class="row">
-                      <div class="col-md-3">
+                  <div className="carousel-item">
+                    <div className="row">
+                      <div className="col-md-3">
                         <a href="#">
-                          <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                          <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                         </a>
                       </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <a href="#">
-                        <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                       </a>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <a href="#">
-                        <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                       </a>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <a href="#">
-                        <img src="http://placehold.it/250x250" alt="" class="maxWidth" />
+                        <img src="http://placehold.it/250x250" alt="" className="maxWidth" />
                       </a>
                     </div>
                   </div>
@@ -207,8 +207,10 @@ class Create extends Component {
           </div>
         </div>
       </div>
+       <nav className="navbar fixed-bottom budgetFooter">
+          <p>Hola</p>
+        </nav> 
       </div>
-      
     )
   }
 }
